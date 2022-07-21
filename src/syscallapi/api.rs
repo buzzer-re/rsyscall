973b3635f1186fabd33a348596dbf40d67f4788a
FallAngel1337 <fallangel@protonmail.com>
2022-07-21 08:12:35 -0300

    Fix todo

--- src/syscallapi/api.rs
+++ src/syscallapi/api.rs
@@ -1,3 +1,4 @@
+use std::process;
 use super::types;
 
 const SYSCALL_API_URL: &str = "https://api.syscall.sh/v1/syscalls/";
@@ -16,23 +17,26 @@ impl SyscallApiClient {
 		let convention_url = format!("{}{}", SYSCALL_CALLE_CONVENTIONS_URL, arch_name);
 		let syscall_url = format!("{}{}", SYSCALL_API_URL, arch_name);
 		
-		// TODO: Review errors output
 		let convention = self.get(&convention_url).unwrap_or_else(|err| {
-			panic!("Error on requesting calling convetion data for {}\nErr: {:?}", arch_name, err);
+			eprintln!("Error on requesting calling convetion data for {}\nErr: {:?}", arch_name, err);
+            process::exit(1);
 		});
 
 		let syscalls = self.get(&syscall_url).unwrap_or_else(|err| {
-			panic!("Error on requesting syscall list for {}\nErr: {:?}", syscall_url, err);
+			eprintln!("Error on requesting syscall list for {}\nErr: {:?}", syscall_url, err);
+            process::exit(1);
 		});
 			
 		// Get calling convention specs
 		let convention = types::Convention::from_str(convention.as_str()).unwrap_or_else(|err| {
-			panic!("Error on parsing calling convetions data => {}", err);
+			eprintln!("Error on parsing calling convetions data => {}", err);
+            process::exit(1);
 		});
 
 
 		let syscalls = types::Syscall::from_str_array(syscalls.as_str()).unwrap_or_else(|err| {
-			panic!("Error on parsing syscall data => {}", err);
+			eprintln!("Error on parsing syscall data => {}", err);
+            process::exit(1);
 		});
 
 		for syscall in syscalls.iter() {
