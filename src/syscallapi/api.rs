c540d5d920d0ab3a262d7527818323e33540d30a
FallAngel1337 <fallangel@protonmail.com>
2022-07-21 08:44:41 -0300

    rustfmt

--- src/syscallapi/api.rs
+++ src/syscallapi/api.rs
@@ -6,44 +6,49 @@ const SYSCALL_CALLE_CONVENTIONS_URL: &str = "https://api.syscall.sh/v1/conventio
 pub struct SyscallApiClient {}
 
 impl SyscallApiClient {
+    pub fn new() -> SyscallApiClient {
+        SyscallApiClient {}
+    }
 
-	pub fn new() -> SyscallApiClient {
-		SyscallApiClient {}
-	}
+    pub fn get_arch_syscall(
+        &self,
+        arch_name: String,
+        syscall_name: String,
+    ) -> Option<(types::Syscall, types::Convention)> {
+        let convention_url = format!("{}{}", SYSCALL_CALLE_CONVENTIONS_URL, arch_name);
+        let syscall_url = format!("{}{}", SYSCALL_API_URL, arch_name);
 
-	pub fn get_arch_syscall(&self, arch_name: String, syscall_name: String) -> Option<(types::Syscall, types::Convention)> {
-		
-		let convention_url = format!("{}{}", SYSCALL_CALLE_CONVENTIONS_URL, arch_name);
-		let syscall_url = format!("{}{}", SYSCALL_API_URL, arch_name);
-		
-		// TODO: Review errors output
-		let convention = self.get(&convention_url).unwrap_or_else(|err| {
-			panic!("Error on requesting calling convetion data for {}\nErr: {:?}", arch_name, err);
-		});
+        // TODO: Review errors output
+        let convention = self.get(&convention_url).unwrap_or_else(|err| {
+            panic!(
+                "Error on requesting calling convetion data for {}\nErr: {:?}",
+                arch_name, err
+            );
+        });
 
-		let syscalls = self.get(&syscall_url).unwrap_or_else(|err| {
-			panic!("Error on requesting syscall list for {}\nErr: {:?}", syscall_url, err);
-		});
-			
-		// Get calling convention specs
-		let convention = types::Convention::from_str(convention.as_str()).unwrap_or_else(|err| {
-			panic!("Error on parsing calling convetions data => {}", err);
-		});
+        let syscalls = self.get(&syscall_url).unwrap_or_else(|err| {
+            panic!(
+                "Error on requesting syscall list for {}\nErr: {:?}",
+                syscall_url, err
+            );
+        });
 
+        // Get calling convention specs
+        let convention = types::Convention::from_str(convention.as_str()).unwrap_or_else(|err| {
+            panic!("Error on parsing calling convetions data => {}", err);
+        });
 
-		let syscalls = types::Syscall::from_str_array(syscalls.as_str()).unwrap_or_else(|err| {
-			panic!("Error on parsing syscall data => {}", err);
-		});
+        let syscalls = types::Syscall::from_str_array(syscalls.as_str()).unwrap_or_else(|err| {
+            panic!("Error on parsing syscall data => {}", err);
+        });
 
-        syscalls.into_iter().find(|s| s.name == syscall_name).map(|s| (s, convention))
+        syscalls
+            .into_iter()
+            .find(|s| s.name == syscall_name)
+            .map(|s| (s, convention))
+    }
 
-	}
-
-
-	fn get(&self, url :&str) -> Result<String, reqwest::Error> {
-		reqwest::blocking::get(url)?.text()
-	}
-
+    fn get(&self, url: &str) -> Result<String, reqwest::Error> {
+        reqwest::blocking::get(url)?.text()
+    }
 }
-
-
